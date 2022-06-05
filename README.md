afaac19bdb1aa191b0f8f8a59f3738017874bcd3
Jay Holt <jholt3@Jays-MacBook-Pro.local>
2022-06-05 13:34:32 -0400

    Added initial psuedocode

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Milestone-Project-1_Browser-Game
+
+This is a Sudoku Puzzle. My first project of the NCSU/Hacker-U Bootcamp
---
+++ index.js
@@ -0,0 +1,11 @@
+// Create function that tracks time  and displays it
+
+// Create function that keeps score and displays it
+
+// Define 9x9 grid numbers (potentially fetch data from API)
+
+// Define correct numbers in 9x9 grid
+
+// Create a boolean function that allows number to be included or rejected depending on accuracy
+
+
