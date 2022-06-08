e9cb8473fc512a28a18c2424b64a86bccb6cdef5
Jay Holt <jholt3@Jays-MacBook-Pro.local>
2022-06-08 08:53:53 -0400

    added grid arrays and slight html and css modifications

--- index.js
+++ index.js
@@ -2,9 +2,32 @@
 
 // Create function that keeps score and displays it
 
-// Define 9x9 grid numbers (potentially fetch data from API)
 
+// Create 9x9 grid numbers (potentially fetch data from API)
+let e = null
+let gameArray = [
+    1, e, e, e, 5, 6, e, e, 9,
+    e, 9, e, e, 1, e, e, e, 3, 
+    4, 7, e, e, e, 9, 5, e, 2,
+    e, e, 5, e, e, e, 8, 6, e,
+    3, 6, e, e, 8, e, e, e, e,
+    e, 8, e, 9, e, e, e, 7, e,
+    8, e, e, 1, 2, e, e, e, 6,
+    e, 5, 4, e, 9, e, 1, 2, e,
+    e, 1, e, 6, e, e, e, e, 8
+]
 // Define correct numbers in 9x9 grid
+let solutionArray = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 
+    5, 9, 8, 7, 1, 2, 6, 4, 3, 
+    4, 7, 6, 8, 3, 9, 5, 1, 2, 
+    9, 4, 5, 2, 7, 3, 8, 6, 1, 
+    3, 6, 7, 5, 8, 1, 2, 9, 4, 
+    2, 8, 1, 9, 6, 4, 3, 7, 5, 
+    8, 3, 9, 1, 2, 7, 4, 5, 6, 
+    6, 5, 4, 3, 9, 8, 1, 2, 7, 
+    7, 1, 2, 6, 4, 5, 9, 3, 8, 
+]
 
 // Create a boolean function that allows number to be included or rejected depending on accuracy
 
